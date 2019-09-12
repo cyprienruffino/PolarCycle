cf5b0d1719b9c7f6ab95f3b042a00cc6c56841db
Cyprien Ruffino <ruffino.cyprien@gmail.com>
2019-09-12 12:26:29 +0200

    Aesthetics + portability

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,3 +1,4 @@
+import progressbar
 import tensorflow as tf
 
 
@@ -8,3 +9,14 @@ def create_weight_histograms(model, name):
         for vect in layer.trainable_weights:
             __weightsumms.append(tf.summary.histogram(name + '_' + layer.name + str(i), vect))
     return __weightsumms
+
+
+def custom_bar(epoch, epoch_iters):
+    return progressbar.ProgressBar(widgets=[
+        "Epoch " + str(epoch), ' ',
+        progressbar.Percentage(), ' ',
+        progressbar.SimpleProgress(format='(%s)' % progressbar.SimpleProgress.DEFAULT_FORMAT),
+        progressbar.Bar(), ' ',
+        progressbar.Timer(), ' ',
+        progressbar.AdaptiveETA()
+    ], maxvalue=epoch_iters, redirect_stdout=True)
